10593cdebb42de88fc80baa4ad3a9d08d0d0f643
dkjb634 <dmitry.kazantsev@jetbrains.com>
2022-10-05 10:37:38 +0300

    no

---
+++ Prac.qtdcore/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Prac.qtdcore
+{
+    public class MainClass
+    {
+        public static bool Foo()
+        {
+            try
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                Console.WriteLine("Read resource: " + assembly.GetManifestResourceNames()[0]);
+                using (Stream stream = assembly.GetManifestResourceStream(
+                    "Prac.qtdcore.data.lists.list.txt.gz"))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string data = reader.ReadToEnd();
+                    Console.WriteLine("data: " + data);
+                }
+            } catch(Exception ex) { Console.WriteLine(ex.Message); return false; }
+            return true;
+
+        }
+        public static void Main(string[] args)
+        {
+            Foo();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestPrj/Test.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System.Reflection;
+using System;
+using System.IO;
+
+namespace TestPrj
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCallFooFromProject()
+        {
+            Console.WriteLine("Read resource through the project reference via foo");
+            if (Prac.qtdcore.MainClass.Foo()) Assert.Pass();
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void TestReadResInUTProject()
+        {
+            Console.WriteLine("Read resource through via linked embedded resource");
+            if (CopiedFoo()) Assert.Pass();
+            Assert.Fail();
+
+        }
+
+        public bool CopiedFoo()
+        {
+            try
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                string listtxtgz = assembly.GetManifestResourceNames()[0];
+                Console.WriteLine("Read resource: " + listtxtgz);
+                using (Stream stream = assembly.GetManifestResourceStream(
+                    "TestPrj.data.lists.list.txt.gz"))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string data = reader.ReadToEnd();
+                    Console.WriteLine("data: " + data);
+                }
+
+                string filenolinktxtgz = assembly.GetManifestResourceNames()[1];
+                Console.WriteLine("Read resource: " + filenolinktxtgz);
+                using (Stream stream = assembly.GetManifestResourceStream(
+                           "TestPrj.data.lists.FileNoLink.txt.gz"))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string data = reader.ReadToEnd();
+                    Console.WriteLine("data: " + data);
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); return false; }
+            return true;
+
+        }
+    }
+}
